571553201f4e9de4844c68feadaed8624e7e55a4
Leandro Matumoto <matumoto.leandro@gmail.com>
2021-02-11 18:44:45 -0300

    Tela Login

---
+++ TelaLogin.py
@@ -0,0 +1,21 @@
+# Canal Dev Aprender
+
+import PySimpleGUI as sg
+
+sg.theme('Reddit')
+layout = [
+    [sg.Text('Usuário'), sg.Input(key='usuario', size = (20,1))],
+    [sg.Text('Senha'), sg.Input(key='senha', password_char='*', size = (20,1))],
+    [sg.Checkbox('Salvar o Login?')],
+    [sg.Button('Entrar')]
+]
+
+janela = sg.Window('Tela de Login', layout)
+
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores['usuario'] == 'User' and valores['senha'] == '1234':
+            print('Bem vindo!')
